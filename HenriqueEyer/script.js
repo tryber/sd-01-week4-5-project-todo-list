91e0dad4646896ef0151f1915660d2f7d6c6d476
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-06 12:00:22 -0300

    Adicionada função onde pega o input de texto e o butao de input

---
+++ HenriqueEyer/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+function return_btn_input(){
+    let btn_input=document.getElementById('btn_input')
+    return btn_input;
+}
+
+function get_txt_input(){
+    let txt_input=document.getElementById('input_txt');
+    return txt_input
+}
