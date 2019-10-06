380fccb1113cf8c09d4ced99b3a6949c58444f03
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-06 12:08:18 -0300

    Criada unica função para pegar os elementos html via parametro id

--- HenriqueEyer/script.js
+++ HenriqueEyer/script.js
@@ -1,16 +1,7 @@
 
 
 
-
-
-
-
-function return_btn_input(){
-    let btn_input=document.getElementById('btn_input')
-    return btn_input;
-}
-
-function get_txt_input(){
-    let txt_input=document.getElementById('input_txt');
-    return txt_input
+function return_element_HTML_by_id(id){
+    let element = document.getElementById(id);
+    return element;
 }
