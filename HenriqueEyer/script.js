cb5ac3c2074de3c984b2cf5335fb73dd93754208
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-06 12:17:27 -0300

    Criada funçoes para criar a estrutura da lista

--- HenriqueEyer/script.js
+++ HenriqueEyer/script.js
@@ -1,6 +1,19 @@
 
 
 
+function add_element_inside_ol(elementfilho,elementpai){
+    elementpai.appendChild(elementfilho);
+}
+
+function add_txt_item_li(element,txt){
+    element.innerHTML=txt;
+}
+
+function create_element_li(){
+    let item_li=document.createElement('li');
+    return item_li;
+}
+
 function return_element_HTML_by_id(id){
     let element = document.getElementById(id);
     return element;
