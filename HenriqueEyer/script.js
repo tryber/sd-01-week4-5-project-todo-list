71dd065b7a06661ddcca3291953ccc385bb24198
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-06 13:47:17 -0300

    Botao para excluir toda a lista pronto, 4 funcoes criadas 1-adicionar o evento de click no botao 2-função para deletar todos os elementos 3-função para deletar o elemento passado
    4- funcao para pegar todos elementos com a tag name

--- HenriqueEyer/script.js
+++ HenriqueEyer/script.js
@@ -1,11 +1,35 @@
 add_event_click_on_btn_input(return_element_HTML_by_id('btn_input'));
+add_event_click_on_btn_excluir(return_element_HTML_by_id('btn_excluir_ALL'))
+
+function add_event_click_on_btn_excluir(btn){
+    btn.addEventListener('click',function(){
+       delete_all_item();
+    })
+}
+
+function delete_all_item(){
+    let list=return_list_by_tag_name('li');
+    let size_list=list.length;
+    let i;
+
+    for(i=0;i<size_list;i++){
+        delete_element(list[0]);
+    }
+
+}
+
+function return_list_by_tag_name(name){
+    let elements=document.getElementsByTagName(name);
+    return elements;
+}
 
 
-
+function delete_element(element){
+    element.parentNode.removeChild(element);
+}
 
 function add_event_click_on_li_for_mark_item_with_line(element){
     element.addEventListener('dblclick', function(){
-        console.log("foi")
         this.style.textDecoration="line-through";
         this.style.color="#808080"
     })
@@ -13,7 +37,7 @@ function add_event_click_on_li_for_mark_item_with_line(element){
 
 function add_event_click_on_li_for_trace_item(element){
     element.addEventListener('click', function(){
-        this.style.background="#F8F8FF	";
+        this.style.background="#F8F8FF";
     })
 }
 
@@ -40,6 +64,12 @@ function add_text_inside_item_li(element,txt){
 function create_element_li(){
     let item_li=document.createElement('li');
     return item_li;
+}
+
+
+function return_element_HTML_by_className(classname){
+    let element = document.getElementsByTagName(classname);
+    return element;
 }
 
 function return_element_HTML_by_id(id){
