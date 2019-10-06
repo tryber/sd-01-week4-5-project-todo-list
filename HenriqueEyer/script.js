c4d5879628cb6d2ea71f411082b945e160a2c97e
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-06 12:52:54 -0300

    Funcao criada para adicionar envento de click no botao de adicionar

--- HenriqueEyer/script.js
+++ HenriqueEyer/script.js
@@ -1,11 +1,19 @@
+add_event_click_on_btn_input(return_element_HTML_by_id('btn_input'));
 
-
+function add_event_click_on_btn_input(element){
+    element.addEventListener('click',function(){
+        let li=create_element_li();
+        let text=return_element_HTML_by_id('input_txt');
+        add_text_inside_item_li(li,text.value);
+        add_element_inside_ol(li,return_element_HTML_by_id('ol_list'));
+    })
+}
 
 function add_element_inside_ol(elementfilho,elementpai){
     elementpai.appendChild(elementfilho);
 }
 
-function add_txt_item_li(element,txt){
+function add_text_inside_item_li(element,txt){
     element.innerHTML=txt;
 }
 
