23c0762087dd6cdfbd81e14564ec11caf5258a27
antoniopaolillo <antonio_francisco_p@live.com>
2019-10-08 17:04:09 -0300

    realizando requisitos e adicionando funcionalidades

--- antoniopaolillo/script.js
+++ antoniopaolillo/script.js
@@ -1,3 +1,4 @@
+var botaoSelecionado;
 function botaoAdicionar() {
     let nomeTarefa = document.getElementById('inputtxt');
     let nomeTarefaValue = nomeTarefa.value;
@@ -17,7 +18,44 @@ function botaoAdicionar() {
             novoElemento.style.textDecoration = "line-through";
         }
     })
+    selecionarElemento(novoElemento);
+}
+
+function selecionarElemento(novoElemento) {
+    novoElemento.addEventListener('click', function () {
+        let elementoAntigo = document.getElementById('botaoselecionado');
+        if (elementoAntigo) {
+            elementoAntigo.setAttribute("id", "");
+        }
+        novoElemento.setAttribute("id", "botaoselecionado");
+        botaoSelecionado = novoElemento;
+    })
+}
+
+function apagarTodosElementos() {
+    let elementoPai = document.getElementsByClassName('containerOl')[0];
+    let numeroDeTarefas = document.getElementsByClassName('tarefa').length - 1;
+
+    for (let i = numeroDeTarefas; i > -1; i--) {
+        let elemento = document.getElementsByClassName('tarefa')[i];
+        elementoPai.removeChild(elemento);
+    }
 
 }
+
+function apagarElementoCompletos() {
+    let todosElementos = document.getElementsByTagName('li').length - 1;
+    let elementoPai = document.getElementsByClassName('containerOl')[0];
+    let elementoCompleto;
+
+    for (let i = todosElementos; i > -1; i--) {
+        elementoCompleto = document.getElementsByTagName('li')[i];
+        if (elementoCompleto.style.textDecoration == "line-through") {
+            elementoPai.removeChild(elementoCompleto);
+        }
+    }
+
+}
+
 
 
