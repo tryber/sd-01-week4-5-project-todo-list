854d043821baad4c076ebd029558146378bfb3e8
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-10-09 18:53:23 -0300

    Save Button

--- bolivar/todo-list.js
+++ bolivar/todo-list.js
@@ -49,10 +49,17 @@ var count0 = 0
 function markclick(item){ 
     item.addEventListener("dblclick", function(){
         count0 += 1
-        if (count0 == 1){
+        if ((count0 == 1) && (this.className !== "marked-item")){
             this.className = "marked-item"
         }
-        if (count0 == 2){
+        else if ((count0 == 1) && (this.className == "marked-item")){
+            this.className = "item"
+        }   
+        else if ((count0 == 2) && (this.className !== "marked-item")){
+            this.className = "marked-item"
+            count0 = 0;
+        }
+        else if ((count0 == 2) && (this.className == "marked-item")){
             this.className = "item"
             count0 = 0;
         }
@@ -60,10 +67,79 @@ function markclick(item){
 }
 
 function clean(){
-
     for (i = 0; i < document.getElementById("main-list").childElementCount; i++){
         var remove = document.getElementById("main-list").childNodes[i]
         document.getElementById("main-list").removeChild(remove)
         i = -1;
     }
 }
+
+function cleanDone(){
+    for (i = 0; i < document.getElementById("main-list").childElementCount; i++){
+        var remove = document.getElementById("main-list").childNodes[i].firstChild
+        var father = document.getElementById("main-list").childNodes[i]
+        if (remove.className == "marked-item"){
+            document.getElementById("main-list").removeChild(father)
+            i = -1;
+        }
+    }
+}
+
+function mouseHouver(){
+    for(i = 0; i < document.getElementsByClassName("button").length; i++){
+        document.getElementsByClassName("button")[i].addEventListener("mouseover", function(){
+            this.style.backgroundColor = "red"
+        })
+        document.getElementsByClassName("button")[i].addEventListener("mouseout", function(){
+            this.style.backgroundColor = "blue"
+        })
+    }
+}
+
+function saveList(){
+    for (i = 0; i < document.getElementById("main-list").childElementCount; i++){
+        var keyitem = "listitem" + i
+        var keyclassName = "listclass" + i
+        var keyid = "listid" + i
+        var content = document.getElementById("main-list").childNodes[i].firstChild.innerHTML
+        var classname = document.getElementById("main-list").childNodes[i].firstChild.className
+        var id = document.getElementById("main-list").childNodes[i].firstChild.id
+        localStorage.setItem(keyitem, content)
+        localStorage.setItem(keyclassName, classname)
+        localStorage.setItem(keyid, id)
+    }
+}
+
+function getList(){
+    for (i = 0; i < localStorage.length/3; i++){
+        var keyitem = "listitem" + i
+        var keyclassName = "listclass" + i
+        var keyid = "listid" + i
+        var item = document.createElement("li");
+        var span = document.createElement("span");
+        item.appendChild(span);
+        document.getElementById("main-list").appendChild(item);
+        span.className = localStorage.getItem(keyclassName)
+        span.id = localStorage.getItem(keyid)
+        span.innerHTML = localStorage.getItem(keyitem)
+    }
+}
+
+function removeSaved(){
+    var limit = localStorage.length/3
+    for (i = 0; i <= limit; i++){
+        var keyitem = "listitem" + i
+        var keyclassName = "listclass" + i
+        var keyid = "listid" + i
+        localStorage.removeItem(keyitem)
+        localStorage.removeItem(keyclassName)
+        localStorage.removeItem(keyid)
+    }
+}
+
+
+window.onload = function () {
+    mouseHouver()
+    getList()
+    removeSaved()
+} 
