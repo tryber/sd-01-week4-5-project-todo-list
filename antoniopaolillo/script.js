b93d041c95774ff73368529b7c09f564ee0a53aa
antoniopaolillo <antonio_francisco_p@live.com>
2019-10-09 17:12:55 -0300

    adicionando funcao local storage e editando css

--- antoniopaolillo/script.js
+++ antoniopaolillo/script.js
@@ -1,10 +1,12 @@
-var botaoSelecionado;
+var tarefaSelecionada;
+var todosElementos;
+var proximoElemento;
+var elementoPai = document.getElementsByClassName('containerOl')[0];
 
 function adicionarTarefa() {
     let nomeTarefa = document.getElementById('inputtxt');
     let nomeTarefaValue = nomeTarefa.value;
     let novoElemento = document.createElement('li');
-    let elementoPai = document.getElementsByClassName('containerOl')[0];
 
     novoElemento.setAttribute("class", "tarefa");
     novoElemento.innerHTML = nomeTarefaValue;
@@ -24,17 +26,16 @@ function adicionarTarefa() {
 
 function selecionarElemento(novoElemento) {
     novoElemento.addEventListener('click', function () {
-        let elementoAntigo = document.getElementById('botaoselecionado');
+        let elementoAntigo = document.getElementById('tarefaSelecionada');
         if (elementoAntigo) {
             elementoAntigo.setAttribute("id", "");
         }
-        novoElemento.setAttribute("id", "botaoselecionado");
-        botaoSelecionado = novoElemento;
+        novoElemento.setAttribute("id", "tarefaSelecionada");
+        tarefaSelecionada = novoElemento;
     })
 }
 
 function apagarTodosElementos() {
-    let elementoPai = document.getElementsByClassName('containerOl')[0];
     let numeroDeTarefas = document.getElementsByClassName('tarefa').length - 1;
 
     for (let i = numeroDeTarefas; i > -1; i--) {
@@ -45,8 +46,7 @@ function apagarTodosElementos() {
 }
 
 function apagarElementosCompletos() {
-    let todosElementos = document.getElementsByTagName('li').length - 1;
-    let elementoPai = document.getElementsByClassName('containerOl')[0];
+    todosElementos = document.getElementsByTagName('li').length - 1;
     let elementoCompleto;
 
     for (let i = todosElementos; i > -1; i--) {
@@ -59,23 +59,41 @@ function apagarElementosCompletos() {
 }
 
 function apagarElementoSelecionado() {
-    let elementoPai = document.getElementsByClassName('containerOl')[0];
-    elementoPai.removeChild(botaoSelecionado);
+    elementoPai.removeChild(tarefaSelecionada);
 }
 
 function subirElemento() {
-    let elementoPai = document.getElementsByClassName('containerOl')[0];
-    let proximoElemento = botaoSelecionado.previousElementSibling;
-
-    elementoPai.insertBefore(botaoSelecionado, proximoElemento);
+    proximoElemento = tarefaSelecionada.previousElementSibling;
+    elementoPai.insertBefore(tarefaSelecionada, proximoElemento);
 }
 
 function descerElemento() {
-    let elementoPai = document.getElementsByClassName('containerOl')[0];
-    let proximoElemento = botaoSelecionado.nextElementSibling;
-
-    elementoPai.insertBefore(proximoElemento, botaoSelecionado);
+    proximoElemento = tarefaSelecionada.nextElementSibling;
+    elementoPai.insertBefore(proximoElemento, tarefaSelecionada);
 }
 
+function salvarLista() {
+    localStorage.clear();
+    todosElementos = document.getElementsByTagName('li').length;
+    let lista = [];
+    for (let i = 0; i < todosElementos; i++) {
+        lista[i] = document.getElementsByTagName('li')[i];
+        localStorage.setItem('lista' + i, lista[i].innerHTML)
+    }
 
+}
+
+function mostrarListaSalva() {
+    let tamanhoLocalStorage = localStorage.length;
+    let criarLi;
+    for (let i = 0; i < tamanhoLocalStorage; i++) {
+        criarLi = document.createElement('li');
+        criarLi.setAttribute("class", "tarefa");
+        criarLi.innerHTML = localStorage['lista' + i];
+        elementoPai.appendChild(criarLi);
+
+    }
+}
+
+mostrarListaSalva();
 
