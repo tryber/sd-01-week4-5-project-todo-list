a5cdccc757e562b875fa8ea86e350017f2c3b4f5
Victor Felipe <vf.ramos7@gmail.com>
2019-10-10 19:32:12 -0300

    finalizado a parte de adicao das lis

--- vrramos/scripts.js
+++ vrramos/scripts.js
@@ -1,16 +1,17 @@
-window.onload = function(){
+window.onload = function() {
     
     function ativarBotao() {
-        let btn
+        let btn;
         
         btn = document.querySelector('#botao-adicionar-tarefa')
-        btn.addEventListener('click', function(){
+        btn.addEventListener('click', function() {
             
             adicionarTarefa()
             
         })
     }
-    function pegarElemento(elemento){
+
+    function pegarElemento(elemento) {
         return document.querySelector(elemento)
     }
     
@@ -21,21 +22,36 @@ window.onload = function(){
     function adicionarElemento(elementoPai, elementoFilho) {
         return elementoPai.appendChild(elementoFilho)
     }
+
+    function inserirComEnter() {
+       let input = pegarElemento('#entrada-tarefa')
+       input.addEventListener('keyup', function(e){
+           if (e.KeyCode == 13) {
+               adicionarTarefa()
+           }
+       })
+    }
     
-    function adicionarTarefa() {        
-        let i, ol, li, listaTarefas, inputTarefa       
+    function criandoListaOl() {      
+        let ol, listaTarefas    
         
         ol = criarElemento('ol')
         listaTarefas = pegarElemento('#lista-de-tarefas')
-        adicionarElemento(listaTarefas,ol)
+        adicionarElemento(listaTarefas,ol)     
+        ol.classList.add('lista-ordenada')                                    
+    }    
+    
+    function adicionarTarefa() {        
+        let ol, li, inputTarefa       
         
-        for( i = 0; i < 1; i++){
-            inputTarefa = pegarElemento('#entrada-tarefa').value        
-            li = criarElemento('li')                 
-            adicionarElemento(ol,li)        
-            li.textContent= inputTarefa
-        }
+        ol = pegarElemento('.lista-ordenada')
+        inputTarefa = pegarElemento('#entrada-tarefa').value        
+        li = criarElemento('li')                
+        adicionarElemento(ol,li)        
+        li.textContent = inputTarefa        
     }
     
+    criandoListaOl()
     ativarBotao()
+    inserirComEnter()
 }
