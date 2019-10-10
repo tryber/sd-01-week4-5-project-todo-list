7ec220b22d20a64b75481c44de616b81023c6e0b
antoniopaolillo <antonio_francisco_p@live.com>
2019-10-10 18:39:04 -0300

    arrumando problema com o localStorage

--- antoniopaolillo/script.js
+++ antoniopaolillo/script.js
@@ -1,6 +1,4 @@
-var tarefaSelecionada;
-var todosElementos;
-var proximoElemento;
+var tarefaSelecionada, todosElementos, proximoElemento;
 var elementoPai = document.getElementsByClassName('containerOl')[0];
 
 function adicionarTarefa() {
@@ -15,17 +13,22 @@ function adicionarTarefa() {
         elementoPai.appendChild(novoElemento);
         nomeTarefa.value = "";
 
-        novoElemento.addEventListener('dblclick', function () {
-            if (novoElemento.style.textDecoration == "line-through") {
-                novoElemento.style.textDecoration = "none";
-            } else {
-                novoElemento.style.textDecoration = "line-through";
-            }
-        })
+        marcarELementoCompleto(novoElemento)
         selecionarElemento(novoElemento);
     } else {
         alert("Faltou o nome, amigão!")
     }
+}
+
+function marcarELementoCompleto(novoElemento) {
+    novoElemento.addEventListener('dblclick', function () {
+        if (novoElemento.style.textDecoration == "line-through") {
+            novoElemento.style.textDecoration = "none";
+        } else {
+            novoElemento.style.textDecoration = "line-through";
+        }
+    })
+
 }
 
 function selecionarElemento(novoElemento) {
@@ -42,7 +45,7 @@ function selecionarElemento(novoElemento) {
 function apagarTodosElementos() {
     let numeroDeTarefas = document.getElementsByClassName('tarefa').length - 1;
 
-    for (let i = numeroDeTarefas; i > -1; i--) {
+    for (let i = numeroDeTarefas; i >= 0; i--) {
         let elemento = document.getElementsByClassName('tarefa')[i];
         elementoPai.removeChild(elemento);
     }
@@ -52,7 +55,7 @@ function apagarElementosCompletos() {
     todosElementos = document.getElementsByTagName('li').length - 1;
     let elementoCompleto;
 
-    for (let i = todosElementos; i > -1; i--) {
+    for (let i = todosElementos; i >= 0; i--) {
         elementoCompleto = document.getElementsByTagName('li')[i];
         if (elementoCompleto.style.textDecoration == "line-through") {
             elementoPai.removeChild(elementoCompleto);
@@ -81,17 +84,30 @@ function salvarLista() {
     for (let i = 0; i < todosElementos; i++) {
         lista[i] = document.getElementsByTagName('li')[i];
         localStorage.setItem('lista' + i, lista[i].innerHTML)
+        //guardando a informação do textDecoration para ser carregada
+        if (lista[i].style.textDecoration) {
+            localStorage.setItem('completo' + i, 1)
+        } else {
+            localStorage.setItem('completo' + i, 0)
+        }
     }
 }
 
 function mostrarListaSalva() {
-    let tamanhoLocalStorage = localStorage.length;
+    let tamanhoLocalStorage = localStorage.length / 2;
     let criarLi;
     for (let i = 0; i < tamanhoLocalStorage; i++) {
         criarLi = document.createElement('li');
         criarLi.setAttribute("class", "tarefa");
         criarLi.innerHTML = localStorage['lista' + i];
         elementoPai.appendChild(criarLi);
+        selecionarElemento(criarLi);
+        marcarELementoCompleto(criarLi);
+
+        //adicionando o textDecoration às tarefas que o tinham
+        if (localStorage['completo' + i] == "1") {
+            criarLi.style.textDecoration = "line-through";
+        }
     }
 }
 
