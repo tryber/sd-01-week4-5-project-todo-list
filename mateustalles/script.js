8d4cffd17495c7429c1f6a9c8b6af4dd2dbbf9b3
Mateus Talles <mateustalles@gmail.com>
2019-10-10 16:27:28 -0300

    Botão apagar criado.

--- mateustalles/script.js
+++ mateustalles/script.js
@@ -8,6 +8,7 @@ function createItem(){
         let orderedList = document.querySelector(".ordered-list")
         orderedList.appendChild(newListItem);
         inputText.value="";
+        inputText.focus();
         selectionCompletion();
     });
 }
@@ -15,16 +16,39 @@ createItem();
 
 function selectionCompletion() {
     let listItems = document.querySelectorAll(".list-item");
-    console.log(listItems)
+    let clickCount = 0;
     for (item of listItems) {
         item.addEventListener("click", function() {
-            console.log(this)
             let itemClassName = this.className;
             if( !itemClassName.includes("active") ) {
                 this.className+=" active";
             } else {
                 this.className+=" complete";
             }
+            if ( itemClassName.includes("complete") ) {
+                clickCount++
+                if (clickCount==2) {
+                    this.className="list-item"
+                }
+                let clickTimeout = setTimeout(function(){ 
+                    clickCount = 0; 
+                    console.log(clickCount) 
+                    clearTimeout(clickTimeout)
+                }, 2000);
+            }
         });
     }
 }
+
+function btnRemove() {
+    let btnRemove = document.getElementById("btn-remove");
+    btnRemove.addEventListener("click", function() {
+        let orderedList = document.querySelector(".ordered-list");
+        let listContainer = document.querySelector(".list-container");
+        let newList = document.createElement("ol");
+        newList.className="ordered-list";
+        orderedList.remove();
+        listContainer.appendChild(newList);        
+    });
+}
+btnRemove();
