9ff5c5592a17dc1ea5f02ae9e4b8a80e044e846c
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-11 12:40:12 -0300

    Deixando o codigo limpo

--- HenriqueEyer/script.js
+++ HenriqueEyer/script.js
@@ -1,21 +1,21 @@
 reloadList();
-addClickBtnInput(returnById('btn_input'));
-addClickBtnExcluir(returnById('btn_excluir_ALL'));
-addClickBtnExcluirItem(returnById('btn_excluir_item'));
-addClickBtnExcluirCompletos(returnById('btn_excluir_completos'));
-addClickBtnSave(returnById('btn_salvar'));
-addClickBtnDown(returnById('btn_down'));
-addClickBtnUP(returnById('btn_up'));
+addClickBtnInput(returnById('btn-input'));
+addClickBtnExcluir(returnById('btn-excluir-ALL'));
+addClickBtnExcluirItem(returnById('btn-excluir-item'));
+addClickBtnExcluirCompletos(returnById('btn-excluir-completos'));
+addClickBtnSave(returnById('btn-salvar'));
+addClickBtnDown(returnById('btn-down'));
+addClickBtnUP(returnById('btn-up'));
 
 
 function addClickBtnUP(element){
-    element.addEventListener('click',function(){
+    element.addEventListener('click', function(){
         moveUp(returnById('select'));
     })
 }
 
 function addClickBtnDown(element){
-    element.addEventListener('click',function(){
+    element.addEventListener('click', function(){
         moveDown(returnById('select'));
     })
 }
@@ -24,18 +24,18 @@ function addClickBtnDown(element){
 
 function moveUp(element){
     if(element){
-        let item_pai=element.parentNode;
-        item_pai.insertBefore(element,element.previousElementSibling);
+        let elementFather = element.parentNode;
+        elementFather.insertBefore(element, element.previousElementSibling);
     }
 }
 
 function moveDown(element){
     if(element){
-        let item_pai=element.parentNode;
-        if(element==item_pai.lastChild){
-            item_pai.insertBefore(element,item_pai.firstElementChild); 
+        let elementFather = element.parentNode;
+        if( element == elementFather.lastChild ){
+            elementFather.insertBefore( element, elementFather.firstElementChild ); 
         }else{
-            item_pai.insertBefore(element.nextElementSibling,element); 
+            elementFather.insertBefore(element.nextElementSibling, element ); 
         }
     }
 }
@@ -45,29 +45,29 @@ function moveDown(element){
 function saveList(){
     localStorage.clear();
     let list = returnByTagName('li') ;
-    let i;
-    for (i=0;i<list.length;i++){
-        if(list[i].textContent!=""){
-            localStorage.setItem('list'+i,list[i].textContent);
+    let position;
+    for ( position = 0; position < list.length; position++ ){
+        if( list[position].textContent != "" ){
+            localStorage.setItem('list'+ position, list[position].textContent);
         }
-        if (list[i].className=="completed") {
-            localStorage.setItem('completed' + i, 1);
+        if ( list[position].className == "completed" ) {
+            localStorage.setItem('completed' + position, 1);
         } else {
-            localStorage.setItem('completed' + i, 0);
+            localStorage.setItem('completed' + position, 0);
         }
     }
 }
 
 function reloadList(){
-    let i;
-    for(i=0;i<localStorage.length/2;i++){
-        let text=localStorage["list"+i];
-        let elementFather =returnById('ol_list');
-        let task=addTextTask(createLI(),text);
-        if (localStorage['completed' + i] == "1") {
+    let position;
+    for( position = 0; position < localStorage.length/2 ; position++ ){
+        let text=localStorage[ "list" + position] ;
+        let elementFather = returnById('ol-list');
+        let task=addTextTask(createLI(), text);
+        if (localStorage['completed' + position] == "1") {
             markCompleted(task);
         }
-        addElementInHTML(task,elementFather);
+        addElementInHTML(task, elementFather);
     }
 }
 
@@ -101,18 +101,18 @@ function addClickBtnExcluirItem(btn){
 
 function deleteAllItem(){
     let list=returnByTagName('li');
-    let size_list=list.length;
-    let i;
-    for(i=0;i<size_list;i++){
+    let sizeList=list.length;
+    let position;
+    for( position = 0; position < sizeList; position++ ){
         deleteElement(list[0]);
     }
 }
 
 function deleteCompleted(){
     let list=returnByClassName('completed');
-    let size_list=list.length;
+    let sizeList=list.length;
     let i;
-    for(i=0;i<size_list;i++){
+    for( i=0; i<sizeList; i++ ){
         deleteElement(list[0]);
     }
     
@@ -141,15 +141,15 @@ function addClickMarkCompleted(element){
 }
 
 function markCompleted(element){
-    if(element.className!="completed"){
-        element.className="completed";
+    if( element.className != "completed" ){
+        element.className = "completed";
     }else{
-        element.className="";
+        element.className = "";
     }
 }
 
 function removeIdSelect(element){
-    element.style.background="";
+    element.style.background = "";
     element.removeAttribute("id");
 }
 
@@ -158,42 +158,42 @@ function addClickLiSelect(element){
     element.addEventListener('click', function(){
         if(returnById('select')){
             removeIdSelect(returnById('select'));
-            this.id="select";
-            this.style.background="#424C55";
+            this.id = "select";
+            this.style.background = "#424C55";
         }else{
-            this.id="select";
-            this.style.background="#424C55";
+            this.id = "select";
+            this.style.background = "#424C55";
         }
     })
 }
 
 function addClickBtnInput(element){
-    element.addEventListener('click',function(){
-        if(returnById('input_txt').value){
+    element.addEventListener('click', function(){
+        if( returnById('input-txt').value ){
             
-            let elementFather =returnById('ol_list');
-            let textInput=returnById('input_txt').value;
-            let taskLi=addTextTask(createLI(),textInput)
-            addElementInHTML(taskLi,elementFather);
-            returnById('input_txt').value="";
+            let elementFather = returnById( 'ol-list' );
+            let textInput = returnById( 'input-txt' ).value;
+            let taskLi = addTextTask(createLI(), textInput)
+            addElementInHTML(taskLi, elementFather);
+            returnById('input-txt').value = "";
         }
     })
 }
 
-function addElementInHTML(elementfilho,elementpai){
-    elementpai.appendChild(elementfilho);
+function addElementInHTML(elementChild , elementFather){
+    elementFather.appendChild(elementChild );
 }
 
-function addTextTask(element,txt){
-    element.innerHTML=txt;
+function addTextTask(element, txt){
+    element.innerHTML = txt;
     addClickLiSelect(element);
     addClickMarkCompleted(element);
     return element;
 }
 
 function createLI(){
-    let item_li=document.createElement('li');
-    return item_li;
+    let elementLi = document.createElement('li');
+    return elementLi;
 }
 
 function returnById(id){
