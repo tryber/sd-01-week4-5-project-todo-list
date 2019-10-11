fe5fa0d11cee64f4c9c37a7fbf8e780d40d5a842
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-10-11 12:53:26 -0300

    projeto incompleto

--- coruja5/script.js
+++ coruja5/script.js
@@ -1,22 +1,24 @@
-//criar tag ol e li para fazer uma listagem de elementos
-//função click terá que ativar este elemento
-//tenho que fazer um createElement para criar sempre um li
-//tachado text-decoration:line-through
+//o codigo esta incompleto mas estou com dúvida em um ponto primordial 
+
+
+
+
 let tag = document.getElementsByTagName("ol")[0];
 let textoInput = document.getElementById("adicionar-tarefa");
 let gerarLista = document.getElementsByClassName("estilo-botoes")[0];
 let indice = 0;
 
 gerarLista.addEventListener('click', function() {
-    let lista = '<li>' + textoInput.value + '</li>';
-    tag.innerHTML += lista;
-    textoInput.value = '';
+    let lista = '<li>' + textoInput.value + '</li>';    //o ponto que estou em dúvida é em relação a esta prática
+    tag.innerHTML += lista;                             //posso realizar desta forma ou é um método inapropriado ?    
+    textoInput.value = '';                  
     textoInput.focus();
     gerarClasse();
 });
 
 function gerarClasse() {
-    let adicionar = document.getElementsByTagName("li")[indice]
-    adicionar.className = "lista-de-tarefas"
-    indice++
+    let adicionar = document.getElementsByTagName("li")[indice];
+    adicionar.className = "lista-de-tarefas";
+    indice++;
 }
+
