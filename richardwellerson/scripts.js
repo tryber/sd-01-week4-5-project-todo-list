f04a76f55cc7494b621cd05322209b2f6318b69f
Richard Wellerson <richardwellerson@gmail.com>
2019-10-11 19:29:39 -0300

    Terminando a logica do for para deletar a lista

--- richardwellerson/scripts.js
+++ richardwellerson/scripts.js
@@ -1,24 +1,35 @@
 
 function textCap() {
-    let addTaskbtn = document.querySelector(".addTask");
+    let addTaskbtn = document.querySelector(".addTask")
     addTaskbtn.addEventListener("click", function () {
     liInsert()
     })
 }
 
 function orderedList() {
-    let genOrdList = document.createElement("ol");
-    let divAdd = document.querySelector(".receiveOl");
+    let genOrdList = document.createElement("ol")
+    let divAdd = document.querySelector(".receiveOl")
     divAdd.appendChild(genOrdList)
 }
 
 function liInsert() {
-    let genLiValue = document.createElement("li");
+    let genLiValue = document.createElement("li")
     let readyTask = document.querySelector(".taskText").value
     let capOl = document.getElementsByTagName("ol")[0]
     capOl.appendChild(genLiValue)
     genLiValue.textContent = readyTask
 }
+
+function cleenTasks(){
+    let btnClear = document.querySelector(".cleanAll")
+    let captureLiTag = document.querySelectorAll("li")
+    let captureOlTag = document.getElementsByTagName("ol")
+    btnClean.addEventListener("click", function() {
+        for(i of captureLiTag){
+            
+        }
+        })
+}   
 
 textCap()
 orderedList()
