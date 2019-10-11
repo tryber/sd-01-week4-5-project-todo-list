51e537575d80974c89a0f9a154babc220d3b468d
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-10-11 00:35:57 -0300

    cursor adicionado

--- coruja5/script.js
+++ coruja5/script.js
@@ -1,3 +1,22 @@
 //criar tag ol e li para fazer uma listagem de elementos
 //função click terá que ativar este elemento
 //tenho que fazer um createElement para criar sempre um li
+
+let tag = document.getElementsByTagName("ol")[0];
+let textoInput = document.getElementById("adicionar-tarefa");
+let gerarLista = document.getElementsByClassName("estilo-botoes")[0];
+let indice = 0;
+
+gerarLista.addEventListener('click', function() {
+    let lista = '<li>' + textoInput.value + '</li>';
+    tag.innerHTML += lista;
+    textoInput.value = '';
+    textoInput.focus();
+    gerarClasse();
+});
+
+function gerarClasse() {
+    let adicionar = document.getElementsByTagName("li")[indice]
+    adicionar.className = "lista-de-tarefas"
+    indice++
+}
