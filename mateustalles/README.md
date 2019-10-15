56ee3824ecb22593d18ab17e056a0df04f538758
Mateus Talles <mateustalles@gmail.com>
2019-10-15 02:56:22 -0300

    Código refatorado, mãozinha sobre os botões, seleção de items completo corrigida, idioma uniformizado, etc.

--- mateustalles/README.md
+++ mateustalles/README.md
@@ -6,6 +6,17 @@ Olá, eis aqui o último projeto de Javascript, que envolve
 criar uma lista de tarefas, ou "To-Do" List com algumas
 interações em Javascript.
 
+Na nova versão, todo o código está refatorado, sem declarações
+de variável em loops, usando funções locais e com idioma e es-
+paçamento uniformizado, além de ter substituido a estrutura da
+função responsável pelos padrões de clique e seleção em switch
+conforme sugerido pelo Alberto! E o cursor se transforma em
+mãozinha sobre os botões! :+1:
+
+Também é possível agora selecionar as atividades completas, bas-
+tando clicar 2x no item para selecionar, e depois clicar apenas
+1x e esperar 1 segundo para o item se tornar selecionado.
+
 Neste projeto também fiz todos os bônus, tentando sempre 
 colocar features adicionais, por exemplo:
 
@@ -22,6 +33,14 @@ depender do caso.
 -Para desmarcar um item dado como completado, o usuário pre-
 cisa realmente dar um duplo-clique dentro de 1 segundo, pois
 usei o recurso timeout para dar mais realismo à atividade.
+
+-Dois botões de remover a tarefa selecionada, um que remove
+apenas na lista, outro que também remove no LocalStorage man-
+tendo a ordem correta.
+
+IMPORTANTE: Sempre que alterar a lista sem usar o botão supra-
+citado, lembre-se de salvá-la, ou a ordem ficará errada quando
+você atualizar.
 
 Enfim, por enquanto é isto que deu para fazer! Espero que 
 curtam!!!
--- mateustalles/script.js
+++ mateustalles/script.js
@@ -1,23 +1,25 @@
 let orderedList = document.querySelector(".ordered-list");
 let inputText = document.getElementById("input-text");
+let itemIndex;
 
 window.addEventListener("load", function() {
     recoverLocalStorage();
     createItem();
-    buttonRemove();
+    buttonRemoveAllAndComplete();
     buttonEraseMemory();
     buttonSave()
-    moveUp();
-    moveDown();
-    removeSelected();
+    buttonMoveUp();
+    buttonMoveDown();
+    buttonRemoveSelected();
+    buttonRemoveSelectedListOnly();
 });
 
 function recoverLocalStorage() {
     let newListItem;
     for (let i = 0; i < localStorage.length/2 ; i++) {    
         newListItem = document.createElement("li");
-        newListItem.className=localStorage.getItem(i+".className");
-        newListItem.innerHTML=localStorage.getItem(i);
+        newListItem.className = localStorage.getItem(i+".className");
+        newListItem.innerHTML = localStorage.getItem(i);
         addToList(orderedList,newListItem);
     };
 };
@@ -27,8 +29,8 @@ function createItem(){
     let newListItem;
     inputSubmit.addEventListener("click", function() {
         newListItem = document.createElement("li")
-        newListItem.className="list-item"
-        newListItem.innerHTML=inputText.value;
+        newListItem.className = "list-item";
+        newListItem.innerHTML = inputText.value;
         addToList(orderedList,newListItem);
     });
 }
@@ -36,9 +38,8 @@ function createItem(){
 function addToList(list,newItem) {
     list.appendChild(newItem);
     inputText.focus();
-    inputText.value="";
+    inputText.value = "";
     selectionPatterns(orderedList);
-
 }
 
 function selectionPatterns(list) {
@@ -47,45 +48,53 @@ function selectionPatterns(list) {
         let itemClassName = this.className;
         switch (itemClassName) {
         case "list-item" :
-            if ( list.contains(document.querySelector("[class$='active']")) ) {
-                let itemAtivo = document.querySelector("[class$='active']");
-                itemAtivo.className = "list-item";
-            };
+            checkForActive();
             this.className += " active";
             break;
+
         case "list-item active" :
-            this.className = "list-item active complete";
+            this.className = "list-item complete";
             break;
-        case "list-item active complete" :
+
+        case "list-item complete active" :
+            this.className = "list-item complete";
+            break;
+
+        case "list-item complete" :
             clickCount++;
-            this.className="list-item active complete selected";
+            checkForActive();
+            this.className = "list-item complete active selected";
             let clickTimeout = setTimeout(function(){ 
                 clickCount = 0; 
-                document.querySelector(".list-item.active.complete.selected").className = "list-item active complete";
-                clearTimeout(clickTimeout)
+                document.querySelector(".list-item.complete.active.selected").className = "list-item complete active";
+                clearTimeout(clickTimeout);
             }, 1000);
             break;
-        case "list-item active complete selected" :
+
+        case "list-item complete active selected" :
             clickCount++
-            if (clickCount==2) { 
-                this.className="list-item"
-                clickCount=0;
+            if (clickCount == 2) { 
+                this.className = "list-item"
+                clickCount = 0;
             };
             break;
         };
    });
+   function checkForActive() {
+        if ( list.contains(document.querySelector(".complete.active")) ) {
+            let completeActiveItem = document.querySelector(".complete.active");
+            completeActiveItem.className = "list-item complete";
+        };
+        if ( list.contains(document.querySelector(".list-item.active")) ) {
+            let activeItem = document.querySelector(".list-item.active");
+            activeItem.className = "list-item";
+        };
+    }
 };
 
-function buttonRemove() {
-    let buttonRemove = document.getElementById("button-remove");
-    buttonRemove.addEventListener("click", function() {
-        let orderedList = document.querySelector(".ordered-list");
-        let listContainer = document.querySelector(".list-container");
-        let newList = document.createElement("ol");
-        newList.className="ordered-list";
-        orderedList.remove();
-        listContainer.appendChild(newList);        
-    });
+function buttonRemoveAllAndComplete() {
+    let buttonRemoveAll = document.getElementById("button-remove");
+    buttonRemoveAll.addEventListener("click", removeAllList);
     let buttonRemoveComplete = document.getElementById("button-remove-complete");
     buttonRemoveComplete.addEventListener("click", function() {
         let completeTasks = document.querySelectorAll("[class$='complete']");
@@ -95,20 +104,29 @@ function buttonRemove() {
     });
 };
 
+function removeAllList () {
+    let taskList = document.querySelectorAll(".list-item");
+    for (let task of taskList) {
+        task.remove();
+    };
+};
+
 function buttonSave() {
     let buttonSave = document.getElementById("button-save");
-    buttonSave.addEventListener("click", function(){
-        if( !!document.querySelectorAll(".list-item") )  {
-            let listItems = document.querySelectorAll(".list-item");
-            let itemIndex;
-            for (item of listItems) {
-                itemIndex = Array.prototype.indexOf.call(listItems, item);
-                localStorage.setItem(itemIndex, item.innerHTML);
-                localStorage.setItem(itemIndex+".className", item.className);
+    buttonSave.addEventListener("click", 
+        refreshLocalStorage);
+};
 
-            };
+function refreshLocalStorage() {
+    localStorage.clear();
+    if( !!document.querySelectorAll(".list-item") )  {
+        let listItems = document.querySelectorAll(".list-item");
+        for (item of listItems) {
+            itemIndex = Array.prototype.indexOf.call(listItems, item);
+            localStorage.setItem(itemIndex, item.innerHTML);
+            localStorage.setItem(itemIndex+".className", item.className);
         };
-    });
+    };
 };
 
 function buttonEraseMemory() {
@@ -116,9 +134,9 @@ function buttonEraseMemory() {
     buttonEraseMemory.addEventListener("click", function(){
     localStorage.clear()
     });
-}
+};
 
-function moveUp() {
+function buttonMoveUp() {
     let buttonMoveUp = document.getElementById("button-moveup");
     buttonMoveUp.addEventListener("click", function() {
         let itemMove = document.querySelector("[class$='active']");
@@ -126,7 +144,7 @@ function moveUp() {
     });
 };
 
-function moveDown() {
+function buttonMoveDown() {
     let buttonMoveUp = document.getElementById("button-movedown");
     buttonMoveUp.addEventListener("click", function() {
         let itemMove = document.querySelector("[class$='active']");
@@ -139,14 +157,23 @@ function moveDown() {
     });
 };
 
-function removeSelected(){
-    let buttonEraseSelected = document.getElementById("button-erase-selected");
+function buttonRemoveSelected(){
+    let buttonEraseSelected = document.getElementById("button-erase-selected-local-storage");
     buttonEraseSelected.addEventListener("click", function() {
         let listItems = document.querySelectorAll(".list-item");
-        let itemSelecionado = document.querySelector("[class$='active']");
-        var itemIndex = Array.prototype.indexOf.call(listItems, itemSelecionado);
+        let selectedItem = document.querySelector("[class*='active']");
+        let itemIndex = Array.prototype.indexOf.call(listItems, selectedItem);
         localStorage.removeItem(itemIndex);
         localStorage.removeItem(itemIndex+".className");
-        itemSelecionado.remove();
+        selectedItem.remove();
+        refreshLocalStorage();
     });
-}
+};
+
+function buttonRemoveSelectedListOnly() {
+    let buttonEraseSelected = document.getElementById("button-erase-selected");
+    buttonEraseSelected.addEventListener("click", function() {
+        let selectedItem = document.querySelector("[class*='active']");
+        selectedItem.remove();
+    });
+};
