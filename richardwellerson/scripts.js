d5b4ba5c3e9e93f4f7edbc91a633a15d0f5bd16e
richardwellerson <richardwellerson@gmail.com>
2019-12-30 17:54:22 -0300

    Criando Funçao de seleçao da tag clicada

--- richardwellerson/scripts.js
+++ richardwellerson/scripts.js
@@ -3,7 +3,7 @@ let buttonClear = document.getElementById("clear-all")
 function textCap() {
     let addTaskbtn = document.querySelector(".add-task")
     addTaskbtn.addEventListener("click", function () {
-    liInsert()
+        liInsert()
     })
 }
 
@@ -21,27 +21,26 @@ function liInsert() {
     genLiValue.textContent = readyTask
 }
 
-function cleenTasks(){
+function cleenTasks() {
     let captureOlTag = document.querySelector("ol")
     let captureLiTag = document.getElementsByTagName("li")
-    buttonClear.addEventListener("click", function() {
-        for(i = 0; i < captureLiTag.length;){
+    buttonClear.addEventListener("click", function () {
+        for (i = 0; i < captureLiTag.length; i) {
             captureOlTag.removeChild(captureLiTag[0])
         }
-        })
-}   
+    })
+}
 
-// function selectTask (){
-//     let arrayTask = document.getElementsByTagName("li")
-//     arrayTask.addEventListener("click", function(){
-//         for(i = 0; i < arrayTask.length; i++){
-//             let backgroundValue = getComputedStyle(arrayTask[i]).getPropertyValue("background-color")
-//             console.log(backgroundValue)
-//         }
-//     })
-// }
+const selectTask = () => {
+    let arrayTask = document.getElementsByTagName("li")
+    for (let i = 0; i < arrayTask.length; i++) {
+        arrayTask[i].addEventListener("click", () => {
+            arrayTask[i].style.color = "green"
+        })
+    }
+}
 
 textCap()
 orderedList()
 cleenTasks()
-// selectTask ()
+selectTask()
