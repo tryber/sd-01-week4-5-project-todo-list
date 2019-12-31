ae01a858f28b94c617b4be616a82c14c7daaad79
richardwellerson <richardwellerson@gmail.com>
2019-12-31 16:42:42 -0300

    Alterando estilo do codigo

--- richardwellerson/scripts.js
+++ richardwellerson/scripts.js
@@ -1,46 +1,46 @@
-let buttonClear = document.getElementById("clear-all")
+let buttonClear = document.getElementById("clear-all");
 
 function textCap() {
-    let addTaskButton = document.querySelector(".add-task")
+    let addTaskButton = document.querySelector(".add-task");
     addTaskButton.addEventListener("click", function () {
-        liInsert()
-    })
+        liInsert();
+    });
 }
 
 function orderedList() {
-    let genOrdList = document.createElement("ol")
-    let divAdd = document.querySelector(".receive-ol")
-    divAdd.appendChild(genOrdList)
+    let genOrdList = document.createElement("ol");
+    let divAdd = document.querySelector(".receive-ol");
+    divAdd.appendChild(genOrdList);
 }
 
 function liInsert() {
-    let genLiValue = document.createElement("li")
-    selectTask(genLiValue)
-    let readyTask = document.querySelector(".task-text").value
-    let capOl = document.getElementsByTagName("ol")[0]
-    capOl.appendChild(genLiValue)
-    genLiValue.textContent = readyTask
+    let genLiValue = document.createElement("li");
+    selectTask(genLiValue);
+    let readyTask = document.querySelector(".task-text").value;
+    let capOl = document.getElementsByTagName("ol")[0];
+    capOl.appendChild(genLiValue);
+    genLiValue.textContent = readyTask;
     document.querySelector(".task-text").value = '';
 }
 
 function cleenTasks() {
-    let captureOlTag = document.querySelector("ol")
-    let captureLiTag = document.getElementsByTagName("li")
+    let captureOlTag = document.querySelector("ol");
+    let captureLiTag = document.getElementsByTagName("li");
     buttonClear.addEventListener("click", function () {
         for (i = 0; i < captureLiTag.length; i) {
-            captureOlTag.removeChild(captureLiTag[0])
+            captureOlTag.removeChild(captureLiTag[0]);
         }
-    })
+    });
 }
 
 const selectTask = (event) => {
     event.addEventListener("click", () => {
         if(event.style.background !== "green"){
-            event.style.background = "green"
+            event.style.background = "green";
         } else {
-            event.style.background = "white"
+            event.style.background = "white";
         }
-    })
+    });
 }
 
 textCap()
